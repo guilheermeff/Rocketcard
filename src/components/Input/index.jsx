0009c749870b9403d42429a9aef90d9c7ac293cf
Guilherme Fernandes <guilheermeff@outlook.com>
2022-10-24 01:18:33 -0400

    added handleSearch function

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,12 +1,27 @@
 import { Container, Button } from './styles';
 
 import { FiSearch } from 'react-icons/fi';
+import { useState } from 'react';
+
+import getUSer from '../../Functions/GithubUser';
 
 export function Input() {
+
+  const [username, setUsername] = useState(" ");
+
+  function handleSearch() {
+    const user = getUSer(username);
+    console.log(user)
+  }
+
   return(
     <Container>
-      <input type="text" placeholder="Username"/>
-      <Button>
+      <input 
+        type="text" 
+        placeholder="Username"
+        onChange={e => setUsername(e.target.value)}
+      />
+      <Button onClick={handleSearch}>
         <FiSearch />
       </Button>
     </Container>
