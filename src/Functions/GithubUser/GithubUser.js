e9beb1a758ebde729716930bb8186babb1851fe6
Guilherme Fernandes <guilheermeff@outlook.com>
2022-10-23 21:05:14 -0400

    create fetch user Github

---
+++ src/Functions/GithubUser/GithubUser.js
@@ -0,0 +1,22 @@
+export class GithubUser {
+
+  static Search(username) {
+
+    const endPoint = `https://api.github.com/users/${username}`;
+
+    return fetch(endPoint)
+    .then(data => data.json())
+    .then(data => {
+      
+      const { login, followers, following, public_repos, location } = data;
+
+      return {
+        login,
+        followers,
+        following,
+        public_repos,
+        location
+      }
+    })
+  }
+}
