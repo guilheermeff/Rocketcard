9ad3573eebcf213861eb8b8392854bbbe61490b9
Guilherme Fernandes <guilheermeff@outlook.com>
2022-11-04 17:00:40 -0400

    remove input function

--- src/components/Infos/index.jsx
+++ src/components/Infos/index.jsx
@@ -1,63 +1,22 @@
 import { useState } from 'react';
-import { Container, List, Li, Button, Box } from "./styles";
+import { Container, List, Li } from "./styles";
 
-import { FiMapPin, FiUsers, FiSearch } from 'react-icons/fi';
+import { FiMapPin, FiUsers} from 'react-icons/fi';
 import { BiBuildings, BiBookBookmark } from 'react-icons/bi';
 
-import getUSer from '../../Functions/GithubUser';
-
-
-export function Input() {
-
-  const [name, setUsername] = useState('');
-
-  function handleSearch() {
-    const user = name;
-
-    const userObject = getUSer(user);
-
-    console.log(userObject)
-
-    const login = userObject.then(data => data.login);
-    const followers = userObject.then(data => data.followers);
-    const following = userObject.then(data => data.following);
-    const location = userObject.then(data => data.location);
-    const public_repos = userObject.then(data => data.public_repos);
-
-    console.log(login)
-    console.log(followers)
-    console.log(following)
-    console.log(location)
-    console.log(public_repos)
-
-    /* CONTINUAR AQUI 01/11 ################# */
-  }
-
-  return(
-    <Box>
-      <input 
-        type="text" 
-        placeholder="Username"
-        onChange={e => setUsername(e.target.value)}
-      />
-      <Button 
-        onClick={handleSearch}
-      >
-        <FiSearch />
-      </Button>
-    </Box>
-  )
-}
-
 export function Infos() {
+
+  const user = localStorage.getItem("user");
+  const userObj = JSON.parse(user)
+
   return(
     <Container>
       <List>
-        <Li> <FiUsers /> 1000 Seguidores </Li>
-        <Li> <FiUsers /> 756 Seguindo </Li>
-        <Li> <BiBookBookmark /> 55 Repositórios </Li>
+        <Li> <FiUsers /> Seguidores </Li>
+        <Li> <FiUsers /> Seguindo </Li>
+        <Li> <BiBookBookmark /> Repositórios </Li>
         <Li> <BiBuildings /> @Rocketseat </Li>
-        <Li> <FiMapPin /> Manaus </Li>
+        <Li> <FiMapPin /></Li>
       </List>
     </Container>
   )
