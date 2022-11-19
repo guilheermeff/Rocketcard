cf65215373c8566efac0d638ae7c4f31c99320f6
Guilherme Fernandes <guilheermeff@outlook.com>
2022-11-19 10:03:18 -0400

    add reload page

--- src/Functions/GithubUser.js
+++ src/Functions/GithubUser.js
@@ -1,12 +1,22 @@
-class GithubUser {
-  static getuser(username) {
+function getUser(username) {
 
-      const endPoint = `https://api.github.com/users/${username}`;
-  
-      return fetch(endPoint)
-              .then(data => data.json())
-              .then(({ login, following, followers, location, public_repos }) => ({ login, following, followers, location, public_repos }))
-  }
+  const endPoint = `https://api.github.com/users/${username}`;
+
+  fetch(endPoint)
+    .then(data => data.json())
+    .then(data => {
+    
+    const { login, following, followers, location, public_repos } = data;
+
+    localStorage.setItem("@Rocketcard:login", login);
+    localStorage.setItem("@Rocketcard:following", following);
+    localStorage.setItem("@Rocketcard:followers", followers);
+    localStorage.setItem("@Rocketcard:location", location);
+    localStorage.setItem("@Rocketcard:public_repos", public_repos);
+  })
+
+  setTimeout(() => location.reload(), 550);
 }
 
-export default GithubUser;
+
+export default getUser;
